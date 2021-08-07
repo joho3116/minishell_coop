2cda82c34fe0a896d6f25334bde5eb93e4703e5c
tbonelee <chanho0325@yonsei.ac.kr>
2021-08-07 22:58:08 +0900

    lex 2(lex.c 컴파일 되는 것까지 확인)

--- parse/lex.c
+++ parse/lex.c
@@ -20,7 +20,7 @@
 // 함수 바깥에서 free등의 처리를 일관성 있게 하기 위해 아무 인자가 없는 빈 스트링이 들어온 경우에도 동적 할당을 하는 쪽으로 생각
 
 
-#include "parse.h"
+#include "minishell.h"
 
 // line이 nul문자를 만나서 정상적으로 while문을 탈출하면 tokenize_return_check에서 만들던 스트링 마무리 지어서 노드에 달아줘야 한다.
 int		tokenize(char *line)
@@ -36,13 +36,15 @@ int		tokenize(char *line)
 			error_check = at_double_quote(&line);
 		else if (*line == '\\' || *line == ';')
 			error_check = SYNTAX_ERROR;
+		else if (*line == '$')
+			error_check = expand_dollar_sign(&line);
 		else if (*line == '>' || *line == '<' || *line == '|')
 			error_check = at_redirection_char(&line);
 		else if (*line == ' ')
 			error_check = at_white_spaces(&line);
-		line++;
 		if (error_check < 0)
 			break ;
+		line++;
 	}
 	return (tokenize_return_check(error_check));
 }
@@ -52,10 +54,175 @@ int		tokenize(char *line)
 int		tokenize_return_check(int status)
 {
 	if (status == 0)
-		return (make_string_and_link_node(FINAL_CHAR, 0));
+		return (make_string_and_link_node(FINAL_WO_CHAR, 0));
 	else
-		return (status);
+		return (make_string_and_link_node(FOR_FREE, 0));
 }
+
+///////////////////////////////////////
+
+int		at_single_quote(char **line)
+{
+	make_string_and_link_node(NORMAL, '\0');
+	while (++(*line))
+	{
+		if (**line == '\'')
+			return (0);
+		else if (**line == '\0')
+			return (SYNTAX_ERROR);
+		make_string_and_link_node(NORMAL, **line);
+	}
+}
+
+int		at_double_quote(char **line)
+{
+	int		error_check;
+
+	error_check = make_string_and_link_node(NORMAL, '\0');
+	while (++(*line))
+	{
+		if (error_check < 0)
+			break ;
+		if (**line == '\"')
+			return (0);
+		else if (**line == '$')
+		{
+			expand_dollar_sign(line);
+			// ++(*line);
+			// if (**line == '?')
+			// 	error_check = expand_exit_status_and_append_string();
+			// else if (ft_isdigit(**line))
+			// 	error_check = 0;
+			// else if (ft_isalpha(**line))
+			// 	error_check = find_env_var_and_mov_ptr(line);
+			// else if (ft_isspace(**line))
+			// 	error_check = make_string_and_link_node(NORMAL, '$');
+		}
+		else if (**line == '\0' || **line == '\\' || **line == ';')
+			error_check = SYNTAX_ERROR;
+		error_check = make_string_and_link_node(NORMAL, **line);
+	}
+	return (error_check);
+}
+
+int		expand_dollar_sign(char **line)
+{
+	++(*line);
+	if (**line == '?')
+		return (expand_exit_status_and_append_string());
+	else if (ft_isdigit(**line))
+		return (0);
+	else if (ft_isalpha(**line))
+		return (find_env_var_and_mov_ptr(line));
+	else if (ft_isspace(**line))
+		return (make_string_and_link_node(NORMAL, '$'));
+}
+
+int		at_redirection_char(char **line)
+{
+	int	error_check;
+
+	error_check = make_string_and_link_node(FINAL_WO_CHAR, 0);
+	if (error_check < 0)
+		return (error_check);
+	if (**line == '>' || **line == '<')
+	{
+		if (**line == *(*line + 1))
+			error_check = make_string_and_link_node(NORMAL, *((*line)++));
+		if (error_check < 0)
+			return (error_check);
+	}
+	error_check = make_string_and_link_node(NORMAL, **line);
+	if (error_check < 0)
+		return (error_check);
+	error_check = make_string_and_link_node(FINAL_WO_CHAR, 0);
+	if (error_check < 0)
+		return (error_check);
+}
+
+int		at_white_spaces(char **line)
+{
+	int	error_check;
+
+	error_check = make_string_and_link_node(FINAL_WO_CHAR, 0);
+	if (error_check < 0)
+		return (error_check);
+	while (ft_isspace(*(++*line)))
+		;
+	(*line)--;
+	return (0);
+}
+///////////////////////////////////////
+
+
+int		expand_exit_status_and_append_string(void)
+{
+	int		error_check;
+	char	*ascii_num;
+	int		idx;
+
+	if (g_info.exit_status == 0)
+	{
+		error_check = make_string_and_link_node(NORMAL, '0');
+		return (error_check);
+	}
+	ascii_num = ft_itoa(g_info.exit_status);
+	if (ascii_num == NULL)
+		return (MALLOC_ERROR);
+	idx = -1;
+	while (ascii_num[++idx] != 0)
+	{
+		error_check = make_string_and_link_node(NORMAL, ascii_num[idx]);
+		if (error_check < 0)
+		{
+			safe_free((void **)&ascii_num);
+			return (error_check);
+		}
+	}
+	safe_free((void **)&ascii_num);
+	return (0);
+}
+
+int		ft_is_in_expansion(char ch)
+{
+	return (!(ft_isspace(ch) || ch == '|' || ch == '>' || ch == '<' || ch == ';' || ch == '/'));
+}
+
+int		find_env_var_and_mov_idx(char **line)
+{
+	char	*start;
+	int		len;
+
+	start = *line;
+	while (!ft_is_in_expansion(*(++(*line))))
+		;
+	len = ((*line)--) - start;
+	return (only_find_env_var(start, len));
+}
+
+int		only_find_env_var(char *start, int len)
+{
+	int	i;
+
+	i = -1;
+	while ((g_info.envp)[++i] != NULL)
+	{
+		if (ft_strncmp(start, (g_info.envp)[i], len) == 0 && (g_info.envp)[i][len] == '=')
+		{
+			while ((g_info.envp)[i][++len] != '\0')
+			{
+				if (make_string_and_link_node(NORMAL, (g_info.envp)[i][len]) < 0)
+					return (MALLOC_ERROR);
+			}
+			return (0);
+		}
+	}
+	return (0);
+}
+
+
+
+
 
 
 // INIT_STRING으로 호출될 때는 항상 그 직전에 string 마무리해서 node에 달아주고 buf는 NULL로 바꿔줬다고 전제..
@@ -68,21 +235,21 @@ int		make_string_and_link_node(int mode, char ch)
 
 	if (mode == FOR_FREE)
 	{
-		safe_free(&buf);
+		safe_free((void **)&buf);
 		return (0);
 	}
 	// 일단 들어온 문자를 buf스트링에 달아준다
-	if (append_char(&buf, ch) < 0)
+	if ((mode != FINAL_WO_CHAR) && (append_char(&buf, ch) < 0))
 		return (MALLOC_ERROR);
 	// 스트링 마무리 지어야 되면 노드 만들어서 달고 리스트에 추가. 그 후 buf에 NULL 할당
-	if (mode == FINAL_CHAR)
+	if (mode == FINAL_W_CHAR || mode == FINAL_WO_CHAR)
 	{
 		if (buf == NULL)
 			return (0);
 		tmp = ft_lstnew((void *)buf);
 		if (tmp == NULL)
 		{
-			safe_free(&buf);
+			safe_free((void **)&buf);
 			return (MALLOC_ERROR);
 		}
 		buf = NULL;
@@ -109,7 +276,7 @@ int		append_char(char **buf, char ch)
 	tmp = malloc(sizeof(char) * (size + 2));
 	if (tmp == NULL)
 	{
-		safe_free(buf);
+		safe_free((void **)buf);
 		return (MALLOC_ERROR);
 	}
 	ft_strlcpy(tmp, *buf, size + 1);
--- parse/parse.h
+++ parse/parse.h
@@ -1,11 +1,10 @@
 #ifndef PARSE_H
 # define PARSE_H
 
-# include "minishell.h"
-
 # define NORMAL 0
-# define FINAL_CHAR 1
-# define FOR_FREE 2
+# define FINAL_W_CHAR 1
+# define FINAL_WO_CHAR 2
+# define FOR_FREE 3
 
 # define END_LINE 0
 # define MALLOC_ERROR -2
@@ -16,8 +15,16 @@ int		tokenize_return_check(int status);
 
 int		at_single_quote(char **line);
 int		at_double_quote(char **line);
+
+int		expand_dollar_sign(char **line);
+
 int		at_redirection_char(char **line);
 int		at_white_spaces(char **line);
+
+
+int		expand_exit_status_and_append_string(void);
+int		find_env_var_and_mov_ptr(char **line);
+int		only_find_env_var(char *start, int len);
 
 int		make_string_and_link_node(int mode, char ch);
 int		append_char(char **buf, char ch);
