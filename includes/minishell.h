11b867c0f7f99ccfa96435c8ac5503c1e7c333ac
tbonelee <chanho0325@yonsei.ac.kr>
2021-08-23 16:13:12 +0900

    echo1(210823, finish, testmain inside echo.c)

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,12 +3,24 @@
 
 # include "../libft/libft.h"
 # include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/stat.h> // S_IRUSR 매크로 위해(norm 위반? 맥에서는 빼도 돌아가니 나중에 빼기)
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/wait.h>
+# include <stdbool.h>
 
 
 # include "../parse/parse.h"
 # include "../utils/utils.h"
+# include "../pipe/pipe.h"
+
+# define OUT_REDIR_PERMISSION_BIT (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+# define STDIN_BACKUP_FD 3
+# define STDOUT_BACKUP_FD 4
 
 # define NORMAL 0
 # define LEX_FINAL_W_CHAR 1
@@ -17,6 +29,11 @@
 
 # define MALLOC_ERROR -2
 # define SYNTAX_ERROR -3
+# define CMD_NOT_FOUND -4
+# define REDIR_INFO_NODE_NOT_FOUND -5
+# define REDIR_INFO_NODE_NULL -6
+# define READ_ERROR -7
+# define OPEN_ERROR -8
 
 # define IN_REDIR 11
 # define OUT_REDIR 12
@@ -24,6 +41,8 @@
 # define OUT_REDIR_APP 14
 # define PIPE 15
 # define NOT_REDIR 16
+
+# define NO_REDIR_FOR_CMD 0
 
 // list_lex_head는 NULL로 초기화
 typedef	struct s_info
