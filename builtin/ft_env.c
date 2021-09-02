50aa886fd30c4bffcc1cc02bde29627cd5f28203
tbonelee <chanho0325@yonsei.ac.kr>
2021-09-02 15:37:16 +0900

    ft_env.c에서 메인문 주석 처리

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: chanlee <chanlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:16:08 by johokyoun         #+#    #+#             */
-/*   Updated: 2021/09/02 15:33:01 by chanlee          ###   ########.fr       */
+/*   Updated: 2021/09/02 15:36:46 by chanlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,40 +35,40 @@ int builtin_env(int i)
     return (0);
 }
 
-t_info g_info;
+// t_info g_info;
 
-int	main(int argc, char *argv[], char *envp[])
-{
-	char	*line;
-	int		error_check;
+// int	main(int argc, char *argv[], char *envp[])
+// {
+// 	char	*line;
+// 	int		error_check;
 
-    error_check = init_minishell_envp(envp);
-	while (1)
-	{
-		line = readline("$> ");
-		error_check = tokenize(line);
-		if (error_check < 0)
-		{
-			print_error(error_check, "");
-			ft_lstclear(&g_info.lex_head, &free);
-			free(line);
-			continue ;
-		}
-		error_check = parse_all();
-		if (error_check < 0)
-		{
-			/*
-			** void free_parse_malloc_in_global_var(void)
-			*/
-			// 전역변수 내 free할 것들 free
-			free_parse_malloc_in_global_var();
-			print_error(error_check, "");
-			free(line);
-			continue ;
-		}
-		int ret = builtin_env(0);
-		printf(" ret = %d\n", ret);
-		free_parse_malloc_in_global_var();
-		free(line);
-	}
-}
+//     error_check = init_minishell_envp(envp);
+// 	while (1)
+// 	{
+// 		line = readline("$> ");
+// 		error_check = tokenize(line);
+// 		if (error_check < 0)
+// 		{
+// 			print_error(error_check, "");
+// 			ft_lstclear(&g_info.lex_head, &free);
+// 			free(line);
+// 			continue ;
+// 		}
+// 		error_check = parse_all();
+// 		if (error_check < 0)
+// 		{
+// 			/*
+// 			** void free_parse_malloc_in_global_var(void)
+// 			*/
+// 			// 전역변수 내 free할 것들 free
+// 			free_parse_malloc_in_global_var();
+// 			print_error(error_check, "");
+// 			free(line);
+// 			continue ;
+// 		}
+// 		int ret = builtin_env(0);
+// 		printf(" ret = %d\n", ret);
+// 		free_parse_malloc_in_global_var();
+// 		free(line);
+// 	}
+// }
